fb179e7aada5ba91a2d62ceba135da1830f9b63c
Wagner1218 <wagnermarin38@gmail.com>
2025-03-11 19:06:36 -0300

    BoardException and defensive programming

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -18,10 +18,10 @@ public class ChessMatch {
 		//função que percorre a matriz board e transforma em uma outra matriz
 		// ChessPiece fazendo um dowcastin
 
-		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColunms()];
+		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
 
 		for (int i = 0; i < board.getRows(); i++) {
-			for (int j = 0; j < board.getColunms(); j++) {
+			for (int j = 0; j < board.getColumns(); j++) {
 
 				mat[i][j] = (ChessPiece) board.piece(i, j);
 
